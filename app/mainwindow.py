0d598bb7015e4c4f199d0cc3bf71c4de942e3521
kBashar <khyrulbashar.0.1@gmail.com>
2022-10-22 15:50:12 +0600

    logger at mainwindow added

--- app/mainwindow.py
+++ app/mainwindow.py
@@ -2,10 +2,16 @@
 import os
 from pathlib import Path
 import sys
+import logging
+from logging.config import dictConfig
 
 from PySide6.QtWidgets import QMainWindow
 from .ui.ui_mainwindow import Ui_MainWindow
+from .projutil.log_conf import DIC_LOGGING_CONFIG
+from .projutil.conf import LOGGER_NAME
 
+dictConfig(DIC_LOGGING_CONFIG)
+logger = logging.getLogger(LOGGER_NAME)
 
 class MainWindow(QMainWindow):
     def __init__(self):
